86d35814af566889bad8eac60282c2452ac647fd
ainok <aino.kurri@aalto.fi>
2023-05-17 16:03:23 +0300

    hamburger menu

--- src/jsfile.js
+++ src/jsfile.js
@@ -1,3 +1,16 @@
+const hamburger = document.getElementById("hamburger")
+const navMenu = document.querySelector(".nav-menu")
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
+
+document.querySelectorAll(".nav-link").forEach(n => n.
+    addEventListener("click", () => {
+        hamburger.classList.remove("active");
+        navMenu.classList.remove("active")
+    }))
 
 let numero = 0
 
