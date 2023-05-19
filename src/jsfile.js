2b94e8404d6292458e556cf89cb19e2c35079e34
Unna Paakkari <unna.paakkari@aalto.fi>
2023-05-19 23:04:34 +0300

    js kuntoon

--- src/jsfile.js
+++ src/jsfile.js
@@ -1,17 +1,17 @@
-const hamburger = document.getElementById("hamburger")
-const navMenu = document.querySelector(".nav-menu")
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.querySelector(".nav-menu");
 
 //navigaatioon liittyvät js-koodit
 hamburger.addEventListener("click", () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
-})
+});
 
 document.querySelectorAll(".nav-link").forEach(n => n.
     addEventListener("click", () => {
         hamburger.classList.remove("active");
-        navMenu.classList.remove("active")
-    }))
+        navMenu.classList.remove("active");
+    }));
 
 //esittely: klikkaamalla ostikkoa video, joka esittelee verkkosivut, alkaa pyörimään
 
@@ -23,19 +23,19 @@ const video = document.getElementById('video');
 
 
 heading.addEventListener('click', () => { 
-    console.log("click")
+    console.log("click");
     video.play(); 
 
     heading.textContent = ''; 
 
     videoContainer.style.display = 'block'; 
-  } 
-) 
+  }
+); 
 
 //numeromuuttujat, joilla lasketaan kierroksia ja pisteitä, joilla kyselyn funktio toimii
-let numero = 0
+let numero = 0;
 
-let loopkierrokset = 0
+let loopkierrokset = 0;
 
 
 const n1 = document.getElementById("N1");
@@ -44,69 +44,69 @@ const n3 = document.getElementById("N3");
 const n4 = document.getElementById("N4");
 const n5 = document.getElementById("N5");
 //jokaiselle kyselyn napille annetaan kyselyn toiminta funktio, ylemmat n1, n2, ja jne ovat nämä napit. add funktio on määritelty rivillä 85
-n1.addEventListener("click", add)
-n2.addEventListener("click", add)
-n3.addEventListener("click", add)
-n4.addEventListener("click", add)
-n5.addEventListener("click", add)
+n1.addEventListener("click", add);
+n2.addEventListener("click", add);
+n3.addEventListener("click", add);
+n4.addEventListener("click", add);
+n5.addEventListener("click", add);
 
 //mobiiliversiossa toimivat nappulat, jotka vaihtaa somesisältöjä
-const vasennappi = document.getElementById("vasennappula")
-const oikeanappi = document.getElementById("oikeanappula")
+const vasennappi = document.getElementById("vasennappula");
+const oikeanappi = document.getElementById("oikeanappula");
 
 //kommentinpoisto nappi
-const poista = document.getElementById("delete")
-poista.addEventListener("click",poistetaan)
+const poista = document.getElementById("delete");
+poista.addEventListener("click",poistetaan);
 
 //kommenttien tykkäykset
-const sydan1 = document.getElementById("sydan-1")
+const sydan1 = document.getElementById("sydan-1");
 
-sydan1.addEventListener("click",tykkaa)
-const sydan2 = document.getElementById("sydan-2")
+sydan1.addEventListener("click",tykkaa);
+const sydan2 = document.getElementById("sydan-2");
 
-sydan2.addEventListener("click",tykkaa)
-const sydan3 = document.getElementById("sydan-3")
+sydan2.addEventListener("click",tykkaa);
+const sydan3 = document.getElementById("sydan-3");
 
-sydan3.addEventListener("click",tykkaa)
+sydan3.addEventListener("click",tykkaa);
 
-const sydan4 = document.getElementById("sydan-4")
+const sydan4 = document.getElementById("sydan-4");
 
-sydan4.addEventListener("click",tykkaa)
+sydan4.addEventListener("click",tykkaa);
 
 //slide (mobiiversion somesisältöä vaihtava funktio) varten luotu muuttuja
-let laskin = 0
+let laskin = 0;
 
 //somesisältöä vaihtavat nappulat: lisätty niille funktio
-vasennappi.addEventListener("click",slide)
-oikeanappi.addEventListener("click",slide)
+vasennappi.addEventListener("click",slide);
+oikeanappi.addEventListener("click",slide);
 
 
 //kyselyn toiminta -funktio: laskee luvun painetusta kyselyn napista ja antaa tuloksen lopuksi niiden perusteella
 function add(element) {
-    console.log(element)
+    console.log(element);
     if (element.target.id == "N1") {
-        numero += 1
+        numero += 1;
     } else if (element.target.id== "N2") {
-        numero += 2
+        numero += 2;
     } else if (element.target.id == "N3") {
-        numero += 3
+        numero += 3;
     } else if  (element.target.id == "N4") {
-        numero += 4
+        numero += 4;
     } else if (element.target.id == "N5") {
-        numero += 5
+        numero += 5;
     }
-    loopkierrokset += 1
+    loopkierrokset += 1;
     if (loopkierrokset == 1) {
-        document.getElementById("kysymys").innerHTML = "2. Kuinka usein huomaat muiden käyttävän puhelinta seurassasi?"
+        document.getElementById("kysymys").innerHTML = "2. Kuinka usein huomaat muiden käyttävän puhelinta seurassasi?";
     } else if (loopkierrokset == 2) {
-        document.getElementById("kysymys").innerHTML = "3. Kuinka usein pidät ryhmätilanteissa puhelimesi pöydällä ruutu ylöspäin?"
+        document.getElementById("kysymys").innerHTML = "3. Kuinka usein pidät ryhmätilanteissa puhelimesi pöydällä ruutu ylöspäin?";
     } else if (loopkierrokset == 3) {
-        document.getElementById("kysymys").innerHTML = "4. Kuinka usein kiinnität huomiota omaan puhelimen käyttöösi muiden seurassa?"
+        document.getElementById("kysymys").innerHTML = "4. Kuinka usein kiinnität huomiota omaan puhelimen käyttöösi muiden seurassa?";
     } else if (loopkierrokset == 4) {
-        document.getElementById("kysymys").innerHTML = "5. Kuinka usein pidät ilmoituksia päällä (myös työskennellessä)?"
+        document.getElementById("kysymys").innerHTML = "5. Kuinka usein pidät ilmoituksia päällä (myös työskennellessä)?";
     } if (loopkierrokset == 5) {
-        document.getElementById("kysymys").innerHTML = "Tuloksesi:"
-        document.getElementById("vastausvaihtoehdot").innerHTML = ""
+        document.getElementById("kysymys").innerHTML = "Tuloksesi:";
+        document.getElementById("vastausvaihtoehdot").innerHTML = "";
         var pallot = document.getElementsByClassName("painikkeet");
         for (var i = 0; i < pallot.length; i++) {pallot[i].innerHTML = "";}          
         if (numero === 0) {document.getElementById("palsu").innerHTML="Yrititkö edes?"}
@@ -124,35 +124,35 @@ function add(element) {
 const nappula = document.getElementById("sendButton");
 
 
-nappula.addEventListener("click", myFunktio)
+nappula.addEventListener("click", myFunktio);
 
 
 //kommentti luodaan keskustelupalstalle
 function myFunktio() {
-    const nimitiedot = document.getElementById("nimi").value
-    const viestitiedot = document.getElementById("viesti").value
-    const kommentti = document.getElementById("omaKommentti")
-    kommentti.style.display ="inline"
-    kommentti.innerHTML = "<p>" + "<b>" + nimitiedot + "</b>" + "<br>" + viestitiedot + "</p>"
-    kommentti.classList.add("aikasempiKokemus")
-    document.getElementById("delete").style.display= "inline"
-    document.getElementById("sydan-4").style.display= "inline"
-    document.getElementById("sydan-4").style.color= "white"
-    nimi.value = ""
-    viesti.value=""
+    const nimitiedot = document.getElementById("nimi").value;
+    const viestitiedot = document.getElementById("viesti").value;
+    const kommentti = document.getElementById("omaKommentti");
+    kommentti.style.display ="inline";
+    kommentti.innerHTML = "<p>" + "<b>" + nimitiedot + "</b>" + "<br>" + viestitiedot + "</p>";
+    kommentti.classList.add("aikasempiKokemus");
+    document.getElementById("delete").style.display= "inline";
+    document.getElementById("sydan-4").style.display= "inline";
+    document.getElementById("sydan-4").style.color= "white";
+    nimi.value = "";
+    viesti.value="";
 }
 
 
 
 //kommentti poistetaan keskustelupalstalta
 function poistetaan() {
-    const kommentti = document.getElementById("omaKommentti")
-    kommentti.innerHTML = ""
-    kommentti.classList.remove("aikaisempiKokemus")
+    const kommentti = document.getElementById("omaKommentti");
+    kommentti.innerHTML = "";
+    kommentti.classList.remove("aikaisempiKokemus");
     kommentti.style.display = "none";
-    poista.style.display ="none"
-    document.getElementById("sydan-4").style.display= "none"
-    document.getElementById("sydan-4").style.color= "white"
+    poista.style.display ="none";
+    document.getElementById("sydan-4").style.display= "none";
+    document.getElementById("sydan-4").style.color= "white";
 }
 
 //ääniteelle toiminta nappulat aloitustarinassa
@@ -175,7 +175,7 @@ function restartAudio() {
 
 function slide(element) {
         if (element.target.id === "oikeanappula") {
-            laskin += 1
+            laskin += 1;
             if (laskin === 0) {
                 document.getElementById("slide-2").style.display = "inline";
                 document.getElementById("slide-1").style.display = "none";
@@ -191,7 +191,7 @@ function slide(element) {
             }
         }
         else if (element.target.id === "vasennappula") {
-            laskin -= 1
+            laskin -= 1;
             if (laskin === 0) {
                 document.getElementById("slide-2").style.display = "inline";
                 document.getElementById("slide-1").style.display = "none";
